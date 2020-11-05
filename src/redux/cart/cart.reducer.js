6f0ab77a89766762f2b23b7bcd0534a1b8315f79
Olfa Ben Yedder <olfabenyedder@hotmail.com>
2020-11-05 13:43:12 +0100

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,25 +1,41 @@
 import CartActionTypes from "./cart-types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
-  cartItems: [],
+  cartItems: []
 };
 
-const cartReducer = (currentStateCart = INITIAL_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
-        ...currentStateCart,
-        hidden: !currentStateCart.hidden,
+        ...state,
+        hidden: !state.hidden,
       };
     case CartActionTypes.ADD_ITEM:
       return {
-        ...currentStateCart,
-        cartItems: addItemToCart(currentStateCart.cartItems, action.payload),
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }
+    //La méthode filter() crée et retourne un nouveau tableau contenant tous les éléments du tableau 
+    //d'origine qui remplissent une condition déterminée par la fonction callback.
+    // retourne un tab contenant cartItem different de payload(à supprimer)
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      }
     default:
-      return currentStateCart;
+      return state;
   }
 };
 
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -18,3 +18,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  // find() renvoie la valeur du premier élément trouvé dans le tableau qui respecte la condition donnée par la 
+  //fonction de test passée en argument. Sinon, la valeur undefined est renvoyée.
+
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  //La méthode filter() crée et retourne un nouveau tableau contenant tous les éléments du tableau 
+  //d'origine qui remplissent une condition déterminée par la fonction callback.
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(
+      cartItem => cartItem.id !== cartItemToRemove.id
+    );
+  }
+
+  //La méthode map() crée un nouveau tableau avec les résultats de l'appel d'une fonction 
+  //fournie sur chaque élément du tableau appelant.
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+}
